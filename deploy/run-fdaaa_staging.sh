83b675ae4e794cec02ef566b236ab144caa418af
Seb Bacon <seb.bacon@gmail.com>
2018-01-18 18:07:21 +0000

    Add WSGI config

--- deploy/run-fdaaa_staging.sh
+++ deploy/run-fdaaa_staging.sh
@@ -2,4 +2,4 @@
 
 cd /var/www/fdaaa_staging/clinicaltrials-act-tracker/clinicaltrials
 
-. /etc/profile.d/fdaaa_staging.sh && exec ../../venv/bin/gunicorn fdaaa_staging.wsgi -c ../deploy/gunicorn-fdaaa.conf.py
+. /etc/profile.d/fdaaa_staging.sh && exec ../../venv/bin/gunicorn fdaaa.wsgi -c ../deploy/gunicorn-fdaaa.conf.py
