881d599db921191d8c2f5d37fd5f15b811ca2279
Seb Bacon <seb.bacon@gmail.com>
2018-01-18 17:59:52 +0000

    More tweaks

--- deploy/run-fdaaa_staging.sh
+++ deploy/run-fdaaa_staging.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd /var/www/fdaaa/clinicaltrials-act-tracker/clinicaltrials
+cd /var/www/fdaaa_staging/clinicaltrials-act-tracker/clinicaltrials
 
 . /etc/profile.d/fdaaa_staging.sh && exec ../../venv/bin/gunicorn fdaaa_staging.wsgi -c ../deploy/gunicorn-fdaaa.conf.py
