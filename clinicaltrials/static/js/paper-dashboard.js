98a0bf0bd3581682db65f815c846976cdbb6556e
Seb Bacon <seb.bacon@gmail.com>
2018-01-24 10:49:56 +0000

    Move nav to top

--- clinicaltrials/static/js/paper-dashboard.js
+++ clinicaltrials/static/js/paper-dashboard.js
@@ -1,17 +1,17 @@
 /*!
-    
+
  =========================================================
  * Paper Dashboard - v1.1.2
  =========================================================
- 
+
  * Product Page: http://www.creative-tim.com/product/paper-dashboard
  * Copyright 2017 Creative Tim (http://www.creative-tim.com)
  * Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard/blob/master/LICENSE.md)
- 
+
  =========================================================
- 
+
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  */
 
 
@@ -19,25 +19,25 @@ var fixedTop = false;
 var transparent = true;
 var navbar_initialized = false;
 
-$(document).ready(function(){
-    window_width = $(window).width();
+// $(document).ready(function(){
+//     window_width = $(window).width();
 
-    // Init navigation toggle for small screens
-    if(window_width <= 991){
-        pd.initRightMenu();
-    }
+//     // Init navigation toggle for small screens
+//     if(window_width <= 991){
+//         pd.initRightMenu();
+//     }
 
-    //  Activate the tooltips
-    $('[rel="tooltip"]').tooltip();
+//     //  Activate the tooltips
+//     $('[rel="tooltip"]').tooltip();
 
-});
+// });
 
-// activate collapse right menu when the windows is resized
-$(window).resize(function(){
-    if($(window).width() <= 991){
-        pd.initRightMenu();
-    }
-});
+// // activate collapse right menu when the windows is resized
+// $(window).resize(function(){
+//     if($(window).width() <= 991){
+//         pd.initRightMenu();
+//     }
+// });
 
 pd = {
     misc:{
@@ -144,14 +144,14 @@ pd = {
 // leading edge, instead of the trailing.
 
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		}, wait);
-		if (immediate && !timeout) func.apply(context, args);
-	};
+        var timeout;
+        return function() {
+                var context = this, args = arguments;
+                clearTimeout(timeout);
+                timeout = setTimeout(function() {
+                        timeout = null;
+                        if (!immediate) func.apply(context, args);
+                }, wait);
+                if (immediate && !timeout) func.apply(context, args);
+        };
 };
