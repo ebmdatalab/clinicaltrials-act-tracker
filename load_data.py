651dc948cd18a4e6cbb730950f18862e3724baf8
Seb Bacon <seb.bacon@gmail.com>
2018-03-01 15:23:47 +0000

    Fix broken config paths

--- load_data.py
+++ load_data.py
@@ -152,11 +152,11 @@ if __name__ == '__main__':
     convert_to_json()
     upload_to_cloud()
     convert_and_download()
-    run(". /etc/profile.d/fdaaa_staging.sh &&  /var/www/fdaaa_staging/venv/bin/python /var/www/fdaaa_staging/clinicaltrials-act-tracker/clinicaltrials/manage.py process_data --input-csv=/tmp/clinical_trials.csv --settings=clinicaltrials.settings")
+    run(". /etc/profile.d/fdaaa_staging.sh &&  /var/www/fdaaa_staging/venv/bin/python /var/www/fdaaa_staging/clinicaltrials-act-tracker/clinicaltrials/manage.py process_data --input-csv=/tmp/clinical_trials.csv --settings=frontend.settings")
     notify_slack("""Today's data uploaded to FDAAA staging: https://staging-fdaaa.ebmdatalab.net.
 
 If this looks good, a dev should run the following on smallweb1:
 
-```. /etc/profile.d/fdaaa.sh &&  /var/www/fdaaa/venv/bin/python /var/www/fdaaa/clinicaltrials-act-tracker/clinicaltrials/manage.py process_data --input-csv=/tmp/clinical_trials.csv --settings=clinicaltrials.settings
+```. /etc/profile.d/fdaaa.sh &&  /var/www/fdaaa/venv/bin/python /var/www/fdaaa/clinicaltrials-act-tracker/clinicaltrials/manage.py process_data --input-csv=/tmp/clinical_trials.csv --settings=frontend.settings
 ```
 """)
