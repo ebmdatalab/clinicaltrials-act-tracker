c53ddb15bb7c5382891cd24311fab73e6d58fdec
Seb Bacon <seb.bacon@gmail.com>
2018-03-07 14:46:30 +0000

    Tidy code

--- clinicaltrials/frontend/tests/test_tweet_today_command.py
+++ clinicaltrials/frontend/tests/test_tweet_today_command.py
@@ -19,7 +19,7 @@ class TweetTodayTestCase(TestCase):
     maxDiff = 6000
     @patch('frontend.trial_computer.date')
     def setUp(self, datetime_mock):
-        self.mock_today = date(2017,1,31)
+        self.mock_today = date(2017, 1, 31)
         datetime_mock.today = Mock(return_value=self.mock_today)
 
     @patch('frontend.management.commands.tweet_today.twitter')
@@ -36,24 +36,24 @@ class TweetTodayTestCase(TestCase):
             self.sponsor,
             results_due=True,
             has_results=True,
-            reported_date=date(2016,12,1),
+            reported_date=date(2016, 12, 1),
             updated_date=self.mock_today)
         for _ in range(3):
             self.late_trial = makeTrial(
                 self.sponsor,
                 results_due=True,
                 has_results=True,
-                reported_date=date(2017,12,1),
+                reported_date=date(2017, 12, 1),
                 updated_date=self.mock_today)
         set_current_rankings()
         api = MagicMock(twitter.api.Api, name='api')
-        #
-        #api_update = MagicMock('api_update')
-        #api.PostUpdate.return_value = api_update
         twitter_mock.Api.return_value = api
         out = StringIO()
         call_command('tweet_today', stdout=out)
-        api.PostUpdate.assert_called_with('Since our last update, 1 trial became overdue, and 3 trials reported late. 1 trial reported its results on time. 80% of all due trials have reported their results.')
+        api.PostUpdate.assert_called_with(
+            'Since our last update, 1 trial became overdue, and 3 trials '
+            'reported late. 1 trial reported its results on time. 80% of '
+            'all due trials have reported their results.')
 
     @patch('frontend.management.commands.tweet_today.twitter')
     def test_single_variable(self, twitter_mock):
@@ -67,13 +67,12 @@ class TweetTodayTestCase(TestCase):
             updated_date=self.mock_today)
         set_current_rankings()
         api = MagicMock(twitter.api.Api, name='api')
-        #
-        #api_update = MagicMock('api_update')
-        #api.PostUpdate.return_value = api_update
         twitter_mock.Api.return_value = api
         out = StringIO()
         call_command('tweet_today', stdout=out)
-        api.PostUpdate.assert_called_with('Since our last update, 1 trial became overdue. 0% of all due trials have reported their results.')
+        api.PostUpdate.assert_called_with(
+            'Since our last update, 1 trial became overdue. 0% of all due'
+            'trials have reported their results.')
 
     @patch('frontend.management.commands.tweet_today.twitter')
     @patch('frontend.views.Ranking')
@@ -81,8 +80,6 @@ class TweetTodayTestCase(TestCase):
         api = MagicMock(twitter.api.Api, name='api')
         latest = ranking_mock.objects.latest.return_value
         latest.date = date(2016, 12, 1)
-        #api_update = MagicMock('api_update')
-        #api.PostUpdate.return_value = api_update
         twitter_mock.Api.return_value = api
         out = StringIO()
         call_command('tweet_today', stdout=out)
