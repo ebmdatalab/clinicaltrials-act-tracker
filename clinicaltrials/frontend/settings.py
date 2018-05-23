3d619f2b3efe0cf22c3204f63feb28257e8c9c04
Seb Bacon <seb.bacon@gmail.com>
2018-05-23 09:31:37 +0100

    Log time

--- clinicaltrials/frontend/settings.py
+++ clinicaltrials/frontend/settings.py
@@ -92,6 +92,11 @@ MIDDLEWARE = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        }
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
