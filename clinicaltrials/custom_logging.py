cac0840ee4967c75dedcc4667503b879a4611f40
Seb Bacon <seb.bacon@gmail.com>
2018-06-21 09:04:45 +0100

    Fix logfile rotation + permissions problem
    Achieved by running update task as www-user, and creating log group-writable
    
    Also some incidental log setting tweaks
    
    Closes #157

---
+++ clinicaltrials/custom_logging.py
@@ -0,0 +1,14 @@
+import logging.handlers
+import os
+
+
+class GroupWriteRotatingFileHandler(logging.handlers.RotatingFileHandler):
+    """A logging filehandler that ensures group writable logs.
+
+    Ref: https://stackoverflow.com/questions/1407474/
+    """
+    def _open(self):
+        umask = os.umask(0o002)
+        handle = logging.handlers.RotatingFileHandler._open(self)
+        os.umask(umask)
+        return handle
