800151533be86239f201ece38ade391b5b5a60f4
Seb Bacon <seb.bacon@gmail.com>
2018-12-11 14:24:33 +0000

    Fix broken migrations

---
+++ clinicaltrials/frontend/migrations/0035_manual_pk_fix.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('frontend', '0032_auto_20180521_1300'),
+    ]
+
+    operations = [
+        migrations.RunSQL('ALTER TABLE frontend_ranking ALTER sponsor_id TYPE varchar(200);'),
+    ]
