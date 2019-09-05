5fc7d8969bae14e389a73deef7e6314289c47400
Seb Bacon <seb.bacon@gmail.com>
2019-09-05 08:01:39 +0100

    Set a very long timout for management commands in staging env

--- deploy/gunicorn-fdaaa_staging.conf.py
+++ deploy/gunicorn-fdaaa_staging.conf.py
@@ -1,3 +1,3 @@
-bind = 'unix:/tmp/gunicorn-fdaaa_staging.sock'
+bind = "unix:/tmp/gunicorn-fdaaa_staging.sock"
 workers = 2
-timeout = 30
+timeout = 6000  # 100 minutes so we have time for the management command to return
