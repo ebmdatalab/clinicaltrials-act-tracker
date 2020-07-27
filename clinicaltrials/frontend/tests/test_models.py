d949bb0cb28d7a8e9b70159746dc2f1610e7debb
Seb Bacon <seb.bacon@gmail.com>
2020-07-27 11:35:30 +0100

    Shift existing tests back two years to avoid a leap year
    This exposed out-by-one bugs in the tests, which this commit also
    fixes, which I presume I overlooked by lazily assuming an
    inclusive/exclusive bug

--- clinicaltrials/frontend/tests/test_models.py
+++ clinicaltrials/frontend/tests/test_models.py
@@ -18,9 +18,9 @@ from unittest.mock import patch, Mock
 
 class RankingTestCase(TestCase):
     def setUp(self):
-        self.date1 = date(2016, 1, 1)
-        self.date2 = date(2016, 2, 1)
-        self.date3 = date(2016, 3, 1)
+        self.date1 = date(2014, 1, 1)
+        self.date2 = date(2014, 2, 1)
+        self.date3 = date(2014, 3, 1)
         self.sponsor1 = Sponsor.objects.create(name="Sponsor 1")
         self.sponsor2 = Sponsor.objects.create(name="Sponsor 2")
         self.sponsor3 = Sponsor.objects.create(name="Sponsor 3")
@@ -91,20 +91,20 @@ class SponsorTrialsTestCase(TestCase):
             registry_id="reported_trial",
             results_due=True,
             has_results=True,
-            reported_date=date(2016, 12, 1),
+            reported_date=date(2014, 12, 1),
         )
         self.qa_submitted_trial = makeTrial(
             self.sponsor, registry_id="qa_submitted_trial", results_due=True
         )
         self.qa_submitted_trial.trialqa_set.create(
-            submitted_to_regulator=date(2016, 12, 1)
+            submitted_to_regulator=date(2014, 12, 1)
         )
         self.cancelled_trial = makeTrial(
             self.sponsor, registry_id="cancelled_trial", results_due=True
         )
         self.cancelled_trial.trialqa_set.create(
-            submitted_to_regulator=date(2016, 12, 1),
-            cancelled_by_sponsor=date(2016, 12, 1),
+            submitted_to_regulator=date(2014, 12, 1),
+            cancelled_by_sponsor=date(2014, 12, 1),
         )
         self.not_due_trial = makeTrial(
             self.sponsor,
@@ -168,10 +168,10 @@ class SponsorTrialsTestCase(TestCase):
             trial.sponsor.save()
         # now alter a couple so we have new "today" state changes
         TrialQA.objects.create(
-            submitted_to_regulator=date(2017, 1, 1), trial_id=self.cancelled_trial.id
+            submitted_to_regulator=date(2015, 1, 2), trial_id=self.cancelled_trial.id
         )
         TrialQA.objects.create(
-            submitted_to_regulator=date(2016, 1, 1), trial_id=self.due_trial.id
+            submitted_to_regulator=date(2014, 1, 2), trial_id=self.due_trial.id
         )
         set_current_rankings()
 
@@ -192,7 +192,7 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(self.sponsor.status_choices(), [("overdue", "Overdue")])
         self.assertEqual(Trial.objects.status_choices(), [("overdue", "Overdue")])
@@ -200,7 +200,7 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=False,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(
             self.sponsor.status_choices(),
@@ -216,7 +216,7 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(trial.status, "overdue")
         self.assertEqual(list(self.sponsor.trial_set.overdue()), [trial])
@@ -226,10 +226,10 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=False,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(trial.status, "ongoing")
-        self.assertEqual(trial.calculated_due_date(), date(2016, 12, 31))
+        self.assertEqual(trial.calculated_due_date(), date(2015, 1, 1))
 
     def test_trial_with_certificate_ongoing(self):
         trial = makeTrial(
@@ -237,18 +237,18 @@ class SponsorTrialsStatusTestCase(TestCase):
             has_results=False,
             results_due=False,
             has_exemption=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(trial.status, "ongoing")
-        self.assertEqual(trial.calculated_due_date(), date(2019, 1, 1))
+        self.assertEqual(trial.calculated_due_date(), date(2017, 1, 1))
 
     def test_trial_not_reported_late(self):
         trial = makeTrial(
             self.sponsor,
             has_results=True,
             results_due=True,
-            completion_date="2016-01-01",
-            reported_date="2016-12-01",
+            completion_date="2014-01-01",
+            reported_date="2014-12-01",
         )
         self.assertEqual(trial.status, "reported")
         self.assertEqual(list(self.sponsor.trial_set.reported_on_time()), [trial])
@@ -259,8 +259,8 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=True,
             results_due=True,
-            completion_date="2016-01-01",
-            reported_date="2017-12-01",
+            completion_date="2014-01-01",
+            reported_date="2015-12-01",
         )
         self.assertEqual(trial.status, "reported-late")
         self.assertEqual(list(self.sponsor.trial_set.reported_on_time()), [])
@@ -271,23 +271,23 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2016-02-01", returned_to_sponsor=None, trial=trial
+            submitted_to_regulator="2014-02-01", returned_to_sponsor=None, trial=trial
         )
         self.assertEqual(trial.status, "reported")
-        self.assertEqual(str(trial.calculated_reported_date()), "2016-02-01")
+        self.assertEqual(str(trial.calculated_reported_date()), "2014-02-01")
 
     def test_overdue_trial_under_qa(self):
         trial = makeTrial(
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2017-02-01", returned_to_sponsor=None, trial=trial
+            submitted_to_regulator="2015-02-01", returned_to_sponsor=None, trial=trial
         )
         self.assertEqual(trial.status, "reported-late")
 
@@ -296,8 +296,8 @@ class SponsorTrialsStatusTestCase(TestCase):
             self.sponsor,
             has_results=True,
             results_due=True,
-            completion_date="2016-01-01",
-            reported_date="2017-01-01",
+            completion_date="2014-01-01",
+            reported_date="2015-01-02",
         )
         self.assertEqual(trial.status, "reported-late")
         self.assertEqual(list(self.sponsor.trial_set.reported_late()), [trial])
@@ -312,8 +312,8 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=True,
             results_due=True,
-            completion_date="2016-01-01",
-            reported_date="2017-01-01",
+            completion_date="2014-01-01",
+            reported_date="2015-01-02",
         )
         self.assertEqual(trial.days_late, 1)
         self.assertEqual(trial.finable_days_late, None)
@@ -323,11 +323,11 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=True,
             results_due=True,
-            completion_date="2015-01-01",
-            reported_date="2017-01-01",
+            completion_date="2014-01-01",
+            reported_date="2016-01-01",
         )
-        self.assertEqual(trial.days_late, 366)
-        self.assertEqual(trial.finable_days_late, 366 - Trial.FINES_GRACE_PERIOD)
+        self.assertEqual(trial.days_late, 365)
+        self.assertEqual(trial.finable_days_late, 365 - Trial.FINES_GRACE_PERIOD)
 
     def test_reported_trial_no_longer_late(self):
         trial = makeTrial(
@@ -335,7 +335,7 @@ class SponsorTrialsLatenessTestCase(TestCase):
             has_results=True,
             results_due=True,
             completion_date="2015-01-01",
-            reported_date="2017-01-01",
+            reported_date="2015-01-01",
         )
         trial.results_due = False
         trial.save()
@@ -346,8 +346,8 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=True,
             results_due=True,
-            completion_date="2016-01-01",
-            reported_date="2016-12-01",
+            completion_date="2014-01-01",
+            reported_date="2014-12-01",
         )
         self.assertEqual(trial.days_late, None)
 
@@ -356,10 +356,10 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2016-02-01", returned_to_sponsor=None, trial=trial
+            submitted_to_regulator="2014-02-01", returned_to_sponsor=None, trial=trial
         )
         self.assertEqual(trial.days_late, None)
 
@@ -368,10 +368,10 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2017-01-01", returned_to_sponsor=None, trial=trial
+            submitted_to_regulator="2015-01-02", returned_to_sponsor=None, trial=trial
         )
         self.assertEqual(trial.days_late, 1)
 
@@ -380,57 +380,46 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2015-01-01",
+            completion_date="2013-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2017-01-01", returned_to_sponsor=None, trial=trial
+            submitted_to_regulator="2015-01-01", returned_to_sponsor=None, trial=trial
         )
-        self.assertEqual(trial.days_late, 366)
-        self.assertEqual(trial.finable_days_late, 336)
+        self.assertEqual(trial.days_late, 365)
+        self.assertEqual(trial.finable_days_late, 335)
 
     @patch("frontend.trial_computer.date")
     def test_trial_under_qa_finably_late_cancelled(self, datetime_mock):
-        datetime_mock.today = Mock(return_value=date(2018, 3, 1))
+        datetime_mock.today = Mock(return_value=date(2016, 3, 1))
         trial = makeTrial(
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2015-01-01",
+            completion_date="2013-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2017-01-01",
-            cancelled_by_sponsor="2018-01-01",
+            submitted_to_regulator="2015-01-01",
+            cancelled_by_sponsor="2016-01-01",
             trial=trial,
         )
         self.assertEqual(trial.days_late, 790)
-        self.assertEqual(trial.finable_days_late, 336)
-
-    def test_reported_trial_finably_late(self):
-        trial = makeTrial(
-            self.sponsor,
-            has_results=True,
-            results_due=True,
-            completion_date="2015-01-01",
-            reported_date="2017-01-01",
-        )
-        self.assertEqual(trial.days_late, 366)
-        self.assertEqual(trial.finable_days_late, 366 - Trial.FINES_GRACE_PERIOD)
+        self.assertEqual(trial.finable_days_late, 335)
 
     @patch("frontend.trial_computer.date")
     def test_trial_under_qa_late_with_cancellation(self, datetime_mock):
-        datetime_mock.today = Mock(return_value=date(2017, 3, 1))
+        datetime_mock.today = Mock(return_value=date(2015, 3, 1))
         trial = makeTrial(
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2016-02-01",
-            cancelled_by_sponsor="2016-02-02",
+            submitted_to_regulator="2014-02-01",
+            cancelled_by_sponsor="2014-02-01",
             trial=trial,
         )
-        self.assertEqual(trial.days_late, 60)
+        self.assertEqual(trial.days_late, 59)
         self.assertEqual(trial.finable_days_late, None)
 
     def test_trial_under_qa_late_with_much_correspondence(self):
@@ -438,40 +427,40 @@ class SponsorTrialsLatenessTestCase(TestCase):
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2016-02-01",
-            returned_to_sponsor="2016-02-02",
+            submitted_to_regulator="2014-02-01",
+            returned_to_sponsor="2014-02-02",
             trial=trial,
         )
         TrialQA.objects.create(
-            submitted_to_regulator="2016-02-03",
-            cancelled_by_sponsor="2016-02-04",
+            submitted_to_regulator="2014-02-03",
+            cancelled_by_sponsor="2014-02-04",
             trial=trial,
         )
-        TrialQA.objects.create(submitted_to_regulator="2017-03-01", trial=trial)
-        self.assertEqual(trial.days_late, 60)
+        TrialQA.objects.create(submitted_to_regulator="2015-03-01", trial=trial)
+        self.assertEqual(trial.days_late, 59)
         self.assertEqual(trial.finable_days_late, None)
 
     @patch("frontend.trial_computer.date")
     def test_unreported_trial_late_within_grace(self, datetime_mock):
-        datetime_mock.today = Mock(return_value=date(2017, 1, 30))
+        datetime_mock.today = Mock(return_value=date(2015, 1, 31))
         trial = makeTrial(
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(trial.days_late, None)
 
     @patch("frontend.trial_computer.date")
     def test_unreported_trial_late_outside_grace(self, datetime_mock):
-        datetime_mock.today = Mock(return_value=date(2017, 1, 31))
+        datetime_mock.today = Mock(return_value=date(2015, 2, 1))
         trial = makeTrial(
             self.sponsor,
             has_results=False,
             results_due=True,
-            completion_date="2016-01-01",
+            completion_date="2014-01-01",
         )
         self.assertEqual(trial.days_late, 31)
